258af3874c18ada99a8f6b026e33b9db19a536d0
betich <betich365@gmail.com>
2025-01-20 22:07:47 +0700

    chore: remove top-level dialog component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
-import { Dialog } from "@/components/ui/dialog";
 import { JobsList, JobsSearch } from "@/modules/jobs";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-sans max-w-screen-lg mx-auto">
-      <Dialog>
-        <JobsSearch />
-        <JobsList />
-      </Dialog>
+      <JobsSearch />
+      <JobsList />
     </div>
   );
 }
